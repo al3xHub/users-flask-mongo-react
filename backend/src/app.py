32a1428e3c6432c61da710aaf0df07fbc47674a1
al3xHub <alejandrobueno92@hotmail.com>
2024-12-10 18:09:49 +0100

    ocultando url db

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,9 +4,13 @@ from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson import json_util, ObjectId
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb://127.0.0.1/usersmongoflaskreact'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
 
 CORS(app)
